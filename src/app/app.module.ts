c7a2ff300a48e0eeceddeca41e09fd0b5d94d434
dani2c <da.cuevasc@duocuc.cl>
2024-08-22 10:06:08 -0400

    1-avance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    HomeComponent,
+    AyudaComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
